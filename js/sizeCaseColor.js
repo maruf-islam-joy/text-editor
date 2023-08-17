2c8edd01fe3ac25e7538b11eef9bb587ae7b3af8
maruf-islam-joy <islam.maruf.lavoro@gmail.com>
2023-08-17 16:17:59 +0200

    added all functionality and footer

---
+++ js/sizeCaseColor.js
@@ -0,0 +1,32 @@
+
+//size action function
+document.getElementById('size-btn').addEventListener('click', function(){
+    const targetArea = getArea();
+    const currentSize = document.getElementById('size-btn').value;
+    targetArea.style.fontSize = currentSize+"px";
+})
+
+
+// case changer function
+let caseCount = 2;
+
+document.getElementById('case-btn').addEventListener('click', function(){
+    const targetArea = getArea();
+    if(caseCount%2 == 0){
+        targetArea.style.textTransform = "uppercase";
+        caseCount++;
+    }
+    else{
+        targetArea.style.textTransform = "none";
+        caseCount++;
+    }
+    
+})
+
+
+// color changer function 
+document.getElementById('color-btn').addEventListener('change',function(e){
+    const targetArea = getArea();
+    let colorNow = e.target.value;
+    targetArea.style.color = colorNow;
+})
